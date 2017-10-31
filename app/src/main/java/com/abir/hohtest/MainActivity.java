5c51d9c329fec79be3f13478f2877424c7965c4e
Abir Lazaar <lazar.abir@gmail.com>
2017-10-31 16:58:23 +0100

    update animation

--- app/src/main/java/com/abir/hohtest/MainActivity.java
+++ app/src/main/java/com/abir/hohtest/MainActivity.java
@@ -5,8 +5,11 @@ import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
+
+import com.jetradarmobile.snowfall.SnowfallView;
 import com.plattysoft.leonids.ParticleSystem;
 
 public class MainActivity extends Activity {
@@ -17,7 +20,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         //bind layout
         setContentView(R.layout.activity_main);
-
         //bind view
         sunrise=(ImageView)findViewById(R.id.sunrise);
         user=(ImageView)findViewById(R.id.user);
@@ -25,45 +27,12 @@ public class MainActivity extends Activity {
         //animation sun rotation
         ObjectAnimator animRotationClockwise;
         animRotationClockwise = ObjectAnimator.ofFloat(sunrise, View.ROTATION, 0f, 360f);
-        animRotationClockwise.setDuration(20000);
+        animRotationClockwise.setDuration(19000);
         animRotationClockwise.setRepeatCount(ValueAnimator.INFINITE);
         animRotationClockwise.setRepeatMode(ValueAnimator.RESTART);
         animRotationClockwise.setInterpolator(new LinearInterpolator());
         //start animation
         animRotationClockwise.start();
-
-        //animation coins snowing
-        //set different rubin icons to animation
-        //change start point for each icon
-        new ParticleSystem(this, 20, R.drawable.img_rubin_one, 10000, R.id.container)
-                .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 0)
-                .setAcceleration(0.00010f, 90)
-                .emit(500,0,4,100000000);
-
-        new ParticleSystem(this, 20, R.drawable.img_rubin_two, 10000, R.id.container_two)
-                .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 0)
-                .setAcceleration(0.00010f, 90)
-                .emit(700,0,4,100000000);
-
-        new ParticleSystem(this, 20, R.drawable.img_rubin_three, 10000, R.id.container_three)
-                .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 0)
-                .setAcceleration(0.00010f, 90)
-                .emit(900,0,6,100000000);
-
-        new ParticleSystem(this, 80, R.drawable.img_rubin_four, 10000, R.id.container_four)
-                .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 0)
-                .setAcceleration(0.00010f, 90)
-                .emit(1100,0,6,100000000);
-
-        new ParticleSystem(this, 20, R.drawable.img_rubin_five, 10000, R.id.container_five)
-                .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 0)
-                .setAcceleration(0.00010f, 90)
-                .emit(200,0,2,100000000);
-
-        new ParticleSystem(this, 20, R.drawable.img_rubin_six, 10000, R.id.container_six)
-                .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 0)
-                .setAcceleration(0.00010f, 90)
-                .emit(1300,0,2,100000000);
 
     }
     
